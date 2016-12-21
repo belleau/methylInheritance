adaf021b35991582574ad70f6bf70e44dc730e8d
adeschen <adeschen@hotmail.com>
2016-12-20 19:38:17 -0500

    Adding functions using methylKitRaw objects

--- R/methylInheritanceMethods.R
+++ R/methylInheritanceMethods.R
@@ -210,6 +210,8 @@ runPermutation <- function(allFilesByGeneration, conditionsByGeneration,
 }
 
 
+
+
 #' @title TODO
 #'
 #' @description TODO
@@ -410,6 +412,188 @@ runRealAnalysis <- function(allFilesByGeneration, conditionsByGeneration,
                             BPPARAM = bp_param)
 
     return(result)
+}
+
+#' @title TODO
+#'
+#' @description TODO
+#'
+#' @param methylKitObject a \code{list} of \code{list}, a \code{list}
+#' composed of \code{list}
+#' of files with methylation information for
+#' bases or regions in the genome. One \code{list} must contain all files
+#' related to the same generation. So, if 3 generations are analyzed, a
+#' \code{list} containing 3 \code{list} must be passed. At least 2 generations
+#' must be present to do a permutation analysis.
+#' The parameter
+#' corresponds to the \code{location} parameter in the package \code{methylKit}.
+#' A \code{list} of \code{vector} containing
+#' \code{0} and \code{1}. The information indicating which files are
+#' associated to controls (\code{0}) and which files are cases (\code{1}).
+#' One \code{vector} must contain all informations
+#' related to the same generation. So, if 3 generations are analyzed, a
+#' \code{list} containing 3 \code{vector} must be passed. At least 2
+#' generations
+#' must be present to do a permutation analysis.
+#' The parameter
+#' corresponds to the \code{treatment} parameter in the package
+#' \code{methylKit}.
+#'
+#' @param output_dir a string, the name of the directory that will contain
+#' the results of the permutation. If the directory does not exist, it will
+#' be created.
+#'
+#' @param nbrPermutations, a positive \code{integer}, the total number of
+#' permutations that is going to be done. Default: \code{1000}.
+#'
+#' @param nbrCores a positive \code{integer}, the number of cores to use when
+#' processing the analysis. Default: \code{1} and always \code{1} for Windows.
+#'
+#' @param nbrCoresDiffMeth a positive \code{integer}, the number of cores
+#' to use for parallel differential methylation calculations.Parameter
+#' used for both sites and tiles analysis. The parameter
+#' corresponds to the \code{num.cores} parameter in the package
+#' \code{methylKit}.
+#' Default: \code{1} and always \code{1} for Windows.
+#'
+#' @param type One of the "sites","tiles" or "both" strings. Specifies the type
+#' of differentially methylated elements should be returned. For
+#' retrieving differentially methylated bases type="sites"; for
+#' differentially methylated regions type="tiles". Default: "both".
+#'
+#' @param minReads a positive \code{integer} Bases and regions having lower
+#' coverage than this count are discarded. The parameter
+#' correspond to the \code{lo.count} parameter in the package \code{methylKit}.
+#'
+#' @param minMethDiff a positive \code{double} betwwen [0,100], the absolute
+#' value of methylation percentage change between cases and controls. The
+#' parameter correspond to the \code{difference} parameter in
+#' the package \code{methylKit}. Default: \code{10}.
+#'
+#' @param qvalue a positive \code{double} betwwen [0,1], the cutoff
+#' for qvalue of differential methylation statistic. Default: \code{0.01}.
+#'
+#' @param maxPercReads a \code{double} between [0,100], the percentile of read
+#' counts that is going to be used as upper cutoff. Bases ore regions
+#' having higher
+#' coverage than this percentile are discarded. Parameter used for both CpG
+#' sites and tiles analysis. The parameter
+#' correspond to the \code{hi.perc} parameter in the package \code{methylKit}.
+#' Default: \code{99.9}.
+#'
+#' @param destrand a \code{logical}, when \code{TRUE} will merge reads on both
+#' strands of a CpG dinucleotide to provide better coverage. Only advised
+#' when looking at CpG methylation. Parameter used for both CpG
+#' sites and tiles analysis.
+#' Default: \code{FALSE}.
+#'
+#' @param minCovBasesForTiles a non-negative \code{integer}, the minimum
+#' number of bases to be covered in a given tiling window. The parameter
+#' corresponds to the \code{cov.bases} parameter in the package
+#' \code{methylKit}.
+#' Only used when \code{doingTiles} =
+#' \code{TRUE}. Default: \code{0}.
+#'
+#' @param tileSize a positive \code{integer}, the size of the tiling window.
+#' The parameter corresponds to the \code{win.size} parameter in
+#' the package \code{methylKit}. Only
+#' used when \code{doingTiles} = \code{TRUE}. Default: \code{1000}.
+#'
+#' @param stepSize a positive \code{integer}, the step size of tiling windows.
+#' The parameter corresponds to the \code{stepSize} parameter in
+#' the package \code{methylKit}. Only
+#' used when \code{doingTiles} = \code{TRUE}. Default: \code{1000}.
+#'
+#' @param vSeed a \code{integer}, a seed used when reproducible results are
+#' needed. When a value inferior or equal to zero is given, a random integer
+#' is used. Default: \code{-1}.
+#'
+#' @return TODO
+#'
+#' @examples
+#'
+#' ##TODO
+#'
+#' @author Astrid Deschenes, Pascal Belleau
+#' @importFrom BiocParallel bplapply MulticoreParam SnowParam bptry bpok bpmapply
+#' @importFrom utils flush.console write.table
+#' @export
+runPermutationUsingRDS <- function(methylKitRDS,
+                           output_dir,
+                           nbrPermutations = 1000,
+                           nbrCores = 1,
+                           nbrCoresDiffMeth = 1,
+                           type = c("both", "sites", "tiles"),
+                           minReads = 10,
+                           minMethDiff = 10,
+                           qvalue = 0.01,
+                           maxPercReads = 99.9,
+                           destrand = FALSE,
+                           minCovBasesForTiles = 0,
+                           tileSize = 1000,
+                           stepSize = 1000,
+                           vSeed = -1) {
+
+    ## Parameters validation
+    validateRunPermutationethylKitObject(methylKitRDS,
+                           output_dir, nbrPermutations,
+                           nbrCores, nbrCoresDiffMeth,
+                           minReads, minMethDiff, qvalue, maxPercReads,
+                           destrand, minCovBasesForTiles, tileSize,
+                           stepSize, vSeed)
+
+    ## Add last slash to path when absent
+    if (substr(output_dir, nchar(output_dir), nchar(output_dir)) != "/") {
+        output_dir <- paste0(output_dir, "/")
+    }
+
+    ## Set vSeed value when negative seed is given
+    if (vSeed <= -1) {
+        tSeed <- as.numeric(Sys.time())
+        vSeed <- 1e8 * (tSeed - floor(tSeed))
+    }
+    set.seed(vSeed)
+
+    methylInfo <- readRDS(methylKitRDS)
+
+    nbGenerations <- length(methylInfo)
+
+    nbSamplesByGeneration <- sapply(methylInfo, length)
+    nbSamples  <- sum(nbSamplesByGeneration)
+    allSamples <- unlist(methylInfo, recursive = FALSE)
+
+    finalList <- vector("list", nbrPermutations)
+
+    for (i in 1:nbrPermutations) {
+
+        # Randomnly mixt all samples
+        samples <- sample(allSamples, size=nbSamples, replace = FALSE)
+
+        permutationList <- vector("list", nbGenerations)
+        start = 1
+        for (j in 1:nbGenerations) {
+            end = start + nbSamplesByGeneration[j] - 1
+            sampleList <- samples[start:end]
+            treatment <- methylInfo[[1]]@treatment
+            newSampleList <- new("methylRawList", sampleList,
+                                    treatment = treatment)
+            permutationList[[j]] <- newSampleList
+            start <- end + 1
+        }
+        finalList[[i]] <- permutationList
+    }
+
+    result <- runOnePermutationOnAllGenerations(finalList[[1]],
+                      nbrCoresDiffMeth = nbrCoresDiffMeth,
+                      type = type,
+                      minReads = minReads,
+                      minMethDiff = minMethDiff,
+                      qvalue = qvalue,
+                      maxPercReads = maxPercReads,
+                      destrand = destrand,
+                      minCovBasesForTiles = minCovBasesForTiles,
+                      tileSize = tileSize,
+                      stepSize = stepSize)
 }
 
 #' @title TODO
