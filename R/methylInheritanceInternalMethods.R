16b4a39e7230f2c87184d7fd139028da9ae8b1bf
adeschen <adeschen@hotmail.com>
2017-01-26 19:12:50 -0500

    Update validateRunPermutationUsingMethylKitInfo() man page

--- R/methylInheritanceInternalMethods.R
+++ R/methylInheritanceInternalMethods.R
@@ -37,15 +37,16 @@
 #'
 #' @param minReads a positive \code{integer} Bases and regions having lower
 #' coverage than this count are discarded. The parameter
-#' correspond to the \code{lo.count} parameter in the  \code{methylKit} package.
+#' corresponds to the \code{lo.count} parameter in the  \code{methylKit}
+#' package.
 #'
 #' @param minMethDiff a positive \code{double} betwwen [0,100], the absolute
 #' value of methylation percentage change between cases and controls. The
-#' parameter correspond to the \code{difference} parameter in
-#' the  \code{methylKit} package.
+#' parameter corresponds to the \code{difference} parameter in
+#' the \code{methylKit} package.
 #'
 #' @param qvalue a positive \code{double} betwwen [0,1], the cutoff
-#' for qvalue of differential methylation statistic.
+#' for qvalue of differential methylation statistic. TODO
 #'
 #' @param maxPercReads a \code{double} between [0,100], the percentile of read
 #' counts that is going to be used as upper cutoff. Bases ore regions
